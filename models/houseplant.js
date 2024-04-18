fdb4bc0a5b0e94635e03a065c5bf79d54e61b299
JoeyM25 <63532048+JoeyM25@users.noreply.github.com>
2024-04-17 22:12:56 -0500

    Final commit

--- models/houseplant.js
+++ models/houseplant.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose")
 const houseplantSchema = mongoose.Schema({
-scientific_name: String,
-common_name: String,
-difficulty: Number
-})
-module.exports = mongoose.model("Houseplant",
-houseplantSchema)
+    scientific_name: String,
+    common_name: String,
+    difficulty: {
+        type: Number,
+        min: 1,
+        max: 10
+    }
+});
+
+module.exports = mongoose.model("Houseplant", houseplantSchema)
 
