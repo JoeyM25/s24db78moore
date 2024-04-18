46d0daeadc4ab9e570f7cfc775b316c8b31e8a20
JoeyM25 <63532048+JoeyM25@users.noreply.github.com>
2024-04-17 21:42:51 -0500

    Added authorization

--- routes/houseplant.js
+++ routes/houseplant.js
@@ -11,8 +11,17 @@ router.get('/detail', houseplant_controllers.houseplant_view_one_page);
 // GET create houseplant page
 router.get('/create', houseplant_controllers.houseplant_create_Page);
 
+// Check if authorized user
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    console.log("Unauthorized user")
+    res.redirect("/login")
+}
+
 // Get create update page
-router.get('/update', houseplant_controllers.houseplant_update_Page);
+router.get('/update', secured, houseplant_controllers.houseplant_update_Page);
 
 // Get delete houseplant page
 router.get('/delete', houseplant_controllers.houseplant_delete_Page);
